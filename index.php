f908b0c37a4ff9bf22b064fb046ebde3fb5f6051
plotiwitolp <plotiwitolp@gmail.com>
2022-08-25 02:43:04 +0400

    first

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+$title = 'Random-Food-Selection';
+require_once './header.php';
+require_once './bd.php';
+?>
+<main>
+
+    <div class="container main">
+        <div class="rand-group">
+            <h2>Первое</h2>
+            <span class="arrow"></span>
+            <div class="circle" id="first">
+                <span class="center"></span>
+            </div>
+            <div class="rand-btn">
+                <button onclick="twister(first);">ПУСК</button>
+            </div>
+        </div>
+        <div class="rand-group">
+            <h2>Второе</h2>
+            <span class="arrow"></span>
+            <div class="circle" id="second">
+                <span class="center"></span>
+            </div>
+            <div class="rand-btn">
+                <button onclick="twister(second);">ПУСК</button>
+            </div>
+        </div>
+        <div class="rand-group">
+            <h2>Десерт</h2>
+            <span class="arrow"></span>
+            <div class="circle" id="dessert">
+                <span class="center"></span>
+            </div>
+            <div class="rand-btn">
+                <button onclick="twister(desserts);">ПУСК</button>
+            </div>
+        </div>
+    </div>
+</main>
+<div class="container">
+    <div class="data">
+        <div id="dishList" data-attr="<?php foreach ($dishes_arr as $v) echo $v . ','; ?>"></div>
+        <div id="dishCatIdList" data-attr="<?php foreach ($dishes_cat_id_arr as $v) echo $v . ','; ?>"></div>
+        <div id="dishCatList" data-attr="<?php foreach ($dishes_categories_arr as $v) echo $v . ','; ?>"></div>
+    </div>
+</div>
+<?php
+require_once './footer.php';
+?>
+<script>
+
+</script>
