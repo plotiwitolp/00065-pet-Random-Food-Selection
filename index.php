5b528cc5a7ac1d02313791f817d2921623b8d3f2
plotiwitolp <plotiwitolp@gmail.com>
2022-08-25 17:14:37 +0400

    add fanfar and deceleration

--- index.php
+++ index.php
@@ -3,9 +3,23 @@ $title = 'Random-Food-Selection';
 require_once './header.php';
 require_once './bd.php';
 ?>
+<div class="data">
+    <div id="dishList" data-attr="<?php foreach ($dishes_arr as $v) echo $v . ','; ?>"></div>
+    <div id="dishCatIdList" data-attr="<?php foreach ($dishes_cat_id_arr as $v) echo $v . ','; ?>"></div>
+    <div id="dishCatList" data-attr="<?php foreach ($dishes_categories_arr as $v) echo $v . ','; ?>"></div>
+</div>
 <main>
 
     <div class="container main">
+
+        <div id="sound_player" style="display:none;">
+            <audio id="player" src="./baraban_1995_hq.mp3"></audio>
+            <audio id="player_winner" src="./winner-of-tour.mp3"></audio>
+            <button id='button_play' onclick="document.getElementById('player').play()"></button>
+            <button id='button_stop' onclick="document.getElementById('player').pause()"></button>
+            <button id='button_play_winner' onclick="document.getElementById('player_winner').play()"></button>
+        </div>
+
         <div class="rand-group">
             <h2>Первое</h2>
             <span class="arrow"></span>
@@ -13,7 +27,7 @@ require_once './bd.php';
                 <span class="center"></span>
             </div>
             <div class="rand-btn">
-                <button onclick="twister(first);">ПУСК</button>
+                <button id="first-btn" onclick="twister(first, firstBtn);">ПУСК</button>
             </div>
         </div>
         <div class="rand-group">
@@ -23,7 +37,7 @@ require_once './bd.php';
                 <span class="center"></span>
             </div>
             <div class="rand-btn">
-                <button onclick="twister(second);">ПУСК</button>
+                <button id="second-btn" onclick="twister(second, secondBtn);">ПУСК</button>
             </div>
         </div>
         <div class="rand-group">
@@ -33,21 +47,31 @@ require_once './bd.php';
                 <span class="center"></span>
             </div>
             <div class="rand-btn">
-                <button onclick="twister(dessert);">ПУСК</button>
+                <button id="dessert-btn" onclick="twister(dessert, dessertBtn);">ПУСК</button>
             </div>
         </div>
     </div>
 </main>
-<div class="container">
-    <div class="data">
-        <div id="dishList" data-attr="<?php foreach ($dishes_arr as $v) echo $v . ','; ?>"></div>
-        <div id="dishCatIdList" data-attr="<?php foreach ($dishes_cat_id_arr as $v) echo $v . ','; ?>"></div>
-        <div id="dishCatList" data-attr="<?php foreach ($dishes_categories_arr as $v) echo $v . ','; ?>"></div>
-    </div>
+
+<div id="div" align="center">
+    <canvas id="canvas" style="border: 0px">
+    </canvas>
 </div>
+
+
 <?php
 require_once './footer.php';
 ?>
+<!-- <script>
+    // function soundClick() {
+    var audio = new Audio(); // Создаём новый элемент Audio
+    audio.src = './priroda.mp3'; // Указываем путь к звуку "клика"
+    audio.autoplay = true; // Автоматически запускаем
+    // }
+    // soundClick();
+</script> -->
+
+
 <script>
 
 </script>
--- script.js
+++ script.js
@@ -25,13 +25,21 @@ for (let i = 0; i < dish.length; i++) {
 const first = document.getElementById('first');
 const second = document.getElementById('second');
 const dessert = document.getElementById('dessert');
+const firstBtn = document.getElementById('first-btn');
+const secondBtn = document.getElementById('second-btn');
+const dessertBtn = document.getElementById('dessert-btn');
+const buttonPlay = document.getElementById('button_play');
+const buttonPlayWinner = document.getElementById('button_play_winner');
+const buttonStop = document.getElementById('button_stop');
+
+let funf;
 
 function createRad(deg, text) {
   const rad = document.createElement('span');
   rad.style.display = 'block';
   rad.style.width = '2px';
-  rad.style.height = '190px';
-  rad.style.backgroundColor = 'yellow';
+  rad.style.height = '170px';
+  rad.style.backgroundColor = '#1dbcc14d';
   rad.style.position = 'absolute';
   rad.style.left = 'calc(50% - 1px)';
   rad.style.top = '50%';
@@ -39,13 +47,10 @@ function createRad(deg, text) {
   rad.style.rotate = `${deg}deg`;
   const b = document.createElement('b');
   b.style.position = 'absolute';
-  //   b.style.width = '180px';
   b.style.fontSize = '14px';
   b.style.top = '0';
-  b.style.right = '85px';
-  //   b.style.transformOrigin = '0% 100%';
+  b.style.right = '75px';
   b.style.color = `white`;
-  //   b.style.rotate = `${deg / 2}deg`;
   rad.append(b);
   b.innerText = text;
   return rad;
@@ -67,13 +72,128 @@ function randomIntFromInterval(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function twister(circle) {
-  const randNumb = randomIntFromInterval(300, 3000);
-  let twistCircle = setInterval(() => {
-    circle.style.rotate = `${chRot++}deg`;
-    if (chRot > randNumb) {
-      clearInterval(twistCircle);
-      chRot = 0;
-    }
-  }, 1);
+function twister(circle, btn) {
+  const randNumb = randomIntFromInterval(1000, 2100);
+  const count = (max, timeout) =>
+    (function next(i) {
+      if (i <= max) {
+        setTimeout(() => {
+          circle.style.rotate = `${i * 3}deg`;
+          // btn.setAttribute('disabled', 'disabled');
+          firstBtn.setAttribute('disabled', 'disabled');
+          secondBtn.setAttribute('disabled', 'disabled');
+          dessertBtn.setAttribute('disabled', 'disabled');
+          buttonPlay.click();
+          clearInterval(funf);
+          next(++i);
+        }, timeout(i));
+      } else {
+        buttonStop.click();
+        buttonPlayWinner.click();
+        fanfare();
+        // btn.removeAttribute('disabled', 'disabled');
+        firstBtn.removeAttribute('disabled', 'disabled');
+        secondBtn.removeAttribute('disabled', 'disabled');
+        dessertBtn.removeAttribute('disabled', 'disabled');
+      }
+    })(0);
+  count(randNumb, (i) => i * 0.02);
 }
+
+function fanfare() {
+  var b = document.getElementById('div');
+  var c = document.getElementById('canvas');
+  var a = c.getContext('2d');
+  var W = (c.width = document.body.clientWidth); //ширина - по размерам клиенской части окна
+  var H = (c.height = document.body.clientHeight);
+  // screen.height; //высота - это не "во весь экран", а больше из-за служебных областей окна
+  var Objects = [];
+  var Colors =
+    '255,0,0;0,255,0;0,0,255;255,255,0;255,0,255;0,255,255;255,255,204;255,204,255;204,255,255'.split(
+      ';'
+    );
+  var timeInterval = 20; //частота обновления, мс
+  var petardColor = 'rgb(255,0,0)'; //цвет петарды до взрыва
+  var numRays = 16; //количество лучей <s>чучхе</s> при взрыве
+  var percentAlive = 60; //процент пускаемых, 0-все, 100-никто
+  var petardRadius = 3; //начальный радиус петарды, пикс.
+  var fireRadius = 31; //радиус для вызрыва, пикс.
+  var fireBallRadius = 5; //радиус отдельного огонька при взрыве, пикс.
+
+  DeleteObject = function (obj, t) {
+    if (t) delete Objects[obj];
+    else Objects[Objects.length] = obj;
+  };
+
+  DrawBack = function () {
+    a['globalCompositeOperation'] = 'source-over'; //новая фигура визуализируется поверх уже добавленных на холст
+    a.fillStyle = 'rgba(0,0,0,.4)';
+    a.fillRect(0, 0, W, H);
+  };
+
+  ColorPath = function (x, y, r, f) {
+    a.beginPath();
+    a.arc(x, y, r, 0, Math.PI * 2, 0);
+    a.fillStyle = f;
+    a.fill();
+  };
+
+  FinalDraw = function (k, x, y, g) {
+    this.k = k;
+    this.x = k ? x : Math.random() * (W - 200) + 100;
+    this.y = k ? y : H;
+    this.t = 0;
+    this.j = k ? 25 : 75;
+    this.a = k ? Math.random() * 360 : 240 + Math.random() * 70;
+    this.s = Math.random() * 3 + 2;
+    this.g = g;
+
+    this.thisDraw = function () {
+      this.t++;
+      if (this.k) {
+        //взрыв
+        f = (Math.PI / 180) * this.a;
+        this.x += Math.cos(f) * this.s;
+        this.y += Math.sin(f) * this.s;
+        a['globalCompositeOperation'] = 'lighter';
+        g = a.createRadialGradient(
+          this.x,
+          this.y,
+          1,
+          this.x,
+          this.y,
+          fireBallRadius
+        );
+        g['addColorStop'](0, 'rgba(255,255,255,.55)');
+        g['addColorStop'](1, 'rgba(' + this.g + ',.03)');
+        ColorPath(this.x, this.y, fireRadius, g);
+      } else {
+        //пуск петарды
+        f = (Math.PI / 180) * this.a;
+        this.x += Math.cos(f) * 5; //
+        this.y += Math.sin(f) * 7.5; //увеличьте для взрывов выше
+        ColorPath(this.x, this.y, petardRadius, petardColor);
+      }
+    };
+  };
+
+  funf = setInterval(function () {
+    DrawBack();
+    for (q in Objects) {
+      var obj = Objects[q];
+      obj.thisDraw();
+      if (obj.t > obj.j) {
+        if (!obj.k) {
+          h = (Math.random() * Colors.length) | 0;
+          for (c = 0; c < numRays; c++)
+            DeleteObject(new FinalDraw(1, obj.x, obj.y, Colors[h]));
+        }
+        DeleteObject(q, 1);
+      }
+    }
+    var c = Math.random() * 100;
+    if (c > percentAlive) DeleteObject(new FinalDraw());
+  }, timeInterval);
+}
+
+fanfare();
